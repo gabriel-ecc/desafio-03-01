7ce93b58353a0835efb5bb7f71112813357f9c91
gabriel <gabriel.castillo.cardenas@gmail.com>
2024-11-20 21:27:00 -0300

    version entregable

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
-precio = 400000
+const precio = 400000
+let contador = 0;
+let totalPago = 0;
 
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio
+
+let botonSumar = document.getElementById('btn-sumar');
+let botonRestar = document.getElementById('btn-restar');
+
+function sumar() {
+  contador = contador + 1;
+  totalPago = contador * precio;
+  let selectorCantidad = document.getElementById('cantidad');
+  selectorCantidad.innerHTML = contador;
+  let selectorTotal = document.getElementById('valor-total');
+  selectorTotal.innerHTML = totalPago;
+}
+
+function restar() {
+    contador = contador - 1;
+    totalPago = contador * precio;
+    let selectorCantidad = document.getElementById('cantidad');
+    selectorCantidad.innerHTML = contador;
+    let selectorTotal = document.getElementById('valor-total');
+    selectorTotal.innerHTML = totalPago;
+}
+
+botonSumar.addEventListener('click', sumar);
+botonRestar.addEventListener('click', restar);
