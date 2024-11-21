f619a566a92e96312f70523738db649dfb64e4fc
ebarruetoc <eduardobarruetosud@gmail.com>
2024-11-20 23:24:48 -0300

    Ultimo cambio, modifiqué la función restar con una condición IF

--- index.js
+++ index.js
@@ -17,19 +17,24 @@ function sumar() {
   selectorTotal.innerHTML = totalPago;
 }
 
-function restar() {
-  if (contador > 0) {
-      contador = contador - 1;
-      totalPago = contador * precio;
-      let selectorCantidad = document.getElementById('cantidad');
-      selectorCantidad.innerHTML = contador;
-      let selectorTotal = document.getElementById('valor-total');
-      selectorTotal.innerHTML = totalPago;
+function restar(){
+  if (contador > 0){
+    contador = contador -1;
+    totalPago = contador * precio;
+    let selectorCantidad = document.getElementById('cantidad');
+    selectorCantidad.innerHTML = contador;
+    let selectorTotal = document.getElementById('valor-total');
+    selectorTotal.innerHTML = totalPago;
   }
+
   else {
-    alert("No puedes restar más.");
+    alert("No puedes restar mas.")
   }
 }
+
+
+
+
 
 botonSumar.addEventListener('click', sumar);
 botonRestar.addEventListener('click', restar);
