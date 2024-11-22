3e5db4b2310dd697c5703b1b9a486ea262696d55
gabriel <gabriel.castillo.cardenas@gmail.com>
2024-11-22 00:00:05 -0300

    simplificacion codigo

--- index.js
+++ index.js
@@ -5,37 +5,30 @@ let totalPago = 0;
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio.toLocaleString('es-ES');
 
-let botonSumar = document.getElementById('btn-sumar');
-let botonRestar = document.getElementById('btn-restar');
 let mostrarError = document.getElementById('texto-error');
+let selectorCantidad = document.getElementById('cantidad');
+let selectorTotal = document.getElementById('valor-total');
 
-function sumar() {
+document.getElementById('btn-sumar').addEventListener('click', ()=>{
   contador = contador + 1;
   totalPago = contador * precio;
-  let selectorCantidad = document.getElementById('cantidad');
   selectorCantidad.innerHTML = contador;
-  let selectorTotal = document.getElementById('valor-total');
   selectorTotal.innerHTML = totalPago.toLocaleString('es-ES');
   mostrarError.innerHTML = ""
   mostrarError.classList.remove("desvanecer");
-}
+});
 
-function restar(){
+document.getElementById('btn-restar').addEventListener('click', ()=>{
   mostrarError.classList.remove("desvanecer");
   mostrarError.innerHTML = ""
   if (contador > 0){
     contador = contador - 1;
     totalPago = contador * precio;
-    let selectorCantidad = document.getElementById('cantidad');
     selectorCantidad.innerHTML = contador;
-    let selectorTotal = document.getElementById('valor-total');
     selectorTotal.innerHTML = totalPago.toLocaleString('es-ES');
   }
   else {
     mostrarError.innerHTML = "Sin elementos"
     mostrarError.classList.add("desvanecer");
   }
-}
-
-botonSumar.addEventListener('click', sumar);
-botonRestar.addEventListener('click', restar);
+});
